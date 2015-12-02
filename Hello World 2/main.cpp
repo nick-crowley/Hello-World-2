8f5ebb51619e817a8605c62a7fde92e9265fdb82
nick-crowley <nicholas.crowley@gmail.com>
2015-12-02 10:36:39 +0000

    Cleaned up main() and project in preparation for release

--- Hello World 2/main.cpp
+++ Hello World 2/main.cpp
@@ -7,9 +7,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "HelloWorld2App.h"   
 
-// Import Common controls v6.0
-//#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 ////////////////////////////////////////////////////////////////////////////////
 // ::_tWinMain
 //! Narrow/wide char entry point
